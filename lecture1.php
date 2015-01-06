7d0baa6a2cafd81e40509a4b6531621ae7e99563
Blake Dayman <daymanblake@gmail.com>
2015-01-06 12:29:13 -0800

    lecture 1 examples

---
+++ lecture1.php
@@ -0,0 +1,47 @@
+<?php
+	class human {
+		//class body
+		public $name = “default name”;
+		public $breed = “best breed”;
+		public $gender = “male”;
+		public $price = 0;
+	}
+	class animal{
+		//class body
+		public $name = “default name”;
+		public $breed = “best breed”;
+		public $gender = “male”;
+		public $price = 0;	
+	}
+	class alien{
+		//class body
+		public $name = “default name”;
+		public $breed = “best breed”;
+		public $gender = “male”;
+		public $price = 0;	
+	}
+	$human1 = new human();
+	print $human1­>name;
+	// default name
+
+	$human2 = new human();
+	print $human2­>name;
+	// default name
+
+	$animal1 = new animal();
+	print $animal1­>name;
+	// default name
+
+	$animal2 = new animal();
+	print $animal2­>name;
+	// default name
+
+	$alien1 = new alien();
+	print $alien1­>name;
+	// default name
+
+	$alien2 = new alienanimal2
+	print $alien2­>name;
+	// default name
+	
+	?>
